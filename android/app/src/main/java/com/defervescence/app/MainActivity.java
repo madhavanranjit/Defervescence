a9e0851c15d2379bce2ef18abd1d82a54f8c1412
JITENDRA KUMAR <jitendrakumar@JITENDRAs-MacBook-Pro.local>
2026-03-21 20:09:40 +0530

    fix WebView settings for network access

--- android/app/src/main/java/com/defervescence/app/MainActivity.java
+++ android/app/src/main/java/com/defervescence/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.defervescence.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+    }
+}
